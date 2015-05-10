c72c2abd1b977118905cbbae6ee555a811ddf793
islam <islammagdy10@gmail.com>
2015-05-09 23:27:58 -0700

    head start

---
+++ chess/chess/Unit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+namespace chess
+{
+   public class Unit
+    {
+       private string name;
+       private int value;
+       private Vector2 postion;
+       private Texture2D unit_texture;
+       private Rectangle unit_rectangle;
+
+       //constractor
+       public Unit(SpriteBatch spritebatch, ContentManager content)
+       {
+
+
+       }
+
+   }
+}
